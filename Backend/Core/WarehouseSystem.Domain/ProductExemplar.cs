6c49b98b1b4226ba7895866c1b4e927392579af9
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-11-12 00:41:11 +0300

    Классы Product, ProductExemplar

---
+++ Backend/Core/WarehouseSystem.Domain/ProductExemplar.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarehouseSystem.Domain
+{
+    public class ProductExemplar
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Code { get; set; }
+
+        public int ProductSKU { get; set; }
+
+        [ForeignKey(nameof(ProductSKU))]
+        public Product Product { get; set; }    
+
+        public int RowNumber { get; set; }
+
+        public int CellNumber { get; set; }
+    }
+}
