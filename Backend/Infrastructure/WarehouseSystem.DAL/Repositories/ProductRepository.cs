5fb29e18f0e20bb0a7cf9094ed718fc60e7d7b2b
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-11-12 11:51:37 +0300

    iProductRepository, ProductRepository

---
+++ Backend/Infrastructure/WarehouseSystem.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using WarehouseSystem.DAL.Interfaces;
+using WarehouseSystem.Domain;
+
+namespace WarehouseSystem.DAL.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly WarehouseContext _context;
+
+        public ProductRepository(WarehouseContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return products;
+        }
+    }
+}
--- Backend/Presentation/WarehouseSystem.WinFormApp/Form1.cs
+++ Backend/Presentation/WarehouseSystem.WinFormApp/Form1.cs
@@ -1,10 +1,20 @@
+using WarehouseSystem.DAL.Interfaces;
+
 namespace WarehouseSystem.WinFormApp
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly IProductRepository _productRepository;
+
+        public Form1(IProductRepository productRepository)
         {
             InitializeComponent();
+            _productRepository = productRepository;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var products = _productRepository.GetProducts();
         }
     }
 }
