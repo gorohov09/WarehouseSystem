c99c8482d4406309b079a4f0143c509c9a0064bf
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-19 23:40:54 +0300

    Интерфейс

--- Backend/Presentation/WarehouseSystem.WinFormApp/Form1.Designer.cs
+++ Backend/Presentation/WarehouseSystem.WinFormApp/Form1.Designer.cs
@@ -28,72 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button_PER_test = new System.Windows.Forms.Button();
-            this.button_inputSKU_test = new System.Windows.Forms.Button();
-            this.textBox_inputSKU = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // dataGridView1
             // 
-            this.button1.Location = new System.Drawing.Point(582, 157);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button_PER_test
-            // 
-            this.button_PER_test.Location = new System.Drawing.Point(582, 192);
-            this.button_PER_test.Name = "button_PER_test";
-            this.button_PER_test.Size = new System.Drawing.Size(94, 29);
-            this.button_PER_test.TabIndex = 1;
-            this.button_PER_test.Text = "PER_TEST";
-            this.button_PER_test.UseVisualStyleBackColor = true;
-            this.button_PER_test.Click += new System.EventHandler(this.button_PER_test_Click);
-            // 
-            // button_inputSKU_test
-            // 
-            this.button_inputSKU_test.Location = new System.Drawing.Point(134, 172);
-            this.button_inputSKU_test.Name = "button_inputSKU_test";
-            this.button_inputSKU_test.Size = new System.Drawing.Size(132, 29);
-            this.button_inputSKU_test.TabIndex = 2;
-            this.button_inputSKU_test.Text = "GetBySKU_TEST";
-            this.button_inputSKU_test.UseVisualStyleBackColor = true;
-            this.button_inputSKU_test.Click += new System.EventHandler(this.button_inputSKU_test_Click);
-            // 
-            // textBox_inputSKU
-            // 
-            this.textBox_inputSKU.Location = new System.Drawing.Point(134, 139);
-            this.textBox_inputSKU.Name = "textBox_inputSKU";
-            this.textBox_inputSKU.Size = new System.Drawing.Size(125, 27);
-            this.textBox_inputSKU.TabIndex = 3;
-            this.textBox_inputSKU.Text = "Введите текст";
-            this.textBox_inputSKU.TextChanged += new System.EventHandler(this.textBox_inputSKU_TextChanged);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1111, 294);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox_inputSKU);
-            this.Controls.Add(this.button_inputSKU_test);
-            this.Controls.Add(this.button_PER_test);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1154, 666);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
-        private Button button_PER_test;
-        private Button button_inputSKU_test;
-        private TextBox textBox_inputSKU;
+        private DataGridView dataGridView1;
     }
 }
