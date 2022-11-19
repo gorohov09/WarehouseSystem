dcb250fe38e045cd7f607bb22c06f2a991679576
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-20 00:29:18 +0300

    Создание второй формы

---
+++ Backend/Presentation/WarehouseSystem.WinFormApp/Forms/FormProductDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WarehouseSystem.Application.Interfaces;
+
+namespace WarehouseSystem.WinFormApp.Forms
+{
+    public partial class FormProductDetails : Form
+    {
+        private readonly IProductService _productService;
+
+        public int ProductId { get; set; }
+
+        public FormProductDetails(IProductService productService)
+        {
+            InitializeComponent();
+            _productService = productService;
+        }
+
+        private void FormProductDetails_Load(object sender, EventArgs e)
+        {
+            var productDetails = _productService.GetProductBySKU(ProductId);
+            label1.Text = productDetails.Name;
+            label2.Text = productDetails.CityProd;
+            label3.Text = productDetails.ProductSKU.ToString();
+        }
+    }
+}
