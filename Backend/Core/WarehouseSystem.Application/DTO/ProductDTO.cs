c5039a32717f295affce5a34a026a3401d6bcc48
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-26 16:29:19 +0300

    Изменил логику

--- Backend/Core/WarehouseSystem.Application/DTO/ProductDTO.cs
+++ Backend/Core/WarehouseSystem.Application/DTO/ProductDTO.cs
@@ -4,22 +4,10 @@ namespace WarehouseSystem.Application.DTO
 {
     public class ProductDTO
     {
-        public int ProductSKU { get; set; }
-
         public string Name { get; set; }
-
-        public string CityProd { get; set; }
-
-        public DateTime DateProd { get; set; }
-
-        public int NumbOfCopies { get; set; }
 
         public decimal PriceProd { get; set; }
 
-        public int VendorNumber { get; set; }
-
-        public bool IsCertificatePresent { get; set; }
-
-        public List<ProductExemplarDTO> ExemplarsDTO { get; set; } = new List<ProductExemplarDTO>(); //свойство навигации
+        public int NumbOfCopies { get; set; }
     }
 }
--- Backend/Core/WarehouseSystem.Application/Services/ProductService.cs
+++ Backend/Core/WarehouseSystem.Application/Services/ProductService.cs
@@ -17,44 +17,18 @@ namespace WarehouseSystem.Application.Services
 
         public ProductDTO GetProductBySKU(int productSKU)
         {
-            var product = _productRepository.GetProductBySKU(productSKU);
-            var productDTO = new ProductDTO
-            {
-                ProductSKU = product.ProductSKU,
-                Name = product.Name,
-                CityProd = product.CityProd,
-                DateProd = product.DateProd,
-                PriceProd = product.PriceProd,
-                IsCertificatePresent = product.IsCertificatePresent,
-                ExemplarsDTO = product.Exemplars.Select(exemplar => new ProductExemplarDTO
-                {
-                    Code = exemplar.Code,
-                    ProductSKU = exemplar.ProductSKU,
-                    RowNumber = exemplar.RowNumber,
-                    CellNumber = exemplar.CellNumber
-                }).ToList()
-            };
-            return productDTO;
+            throw new NotImplementedException();
         }
 
         public IEnumerable<ProductDTO> GetProducts()
         {
             var products = _productRepository.GetProducts();
+
             var productsDTO = products.Select(product => new ProductDTO
             {
-                ProductSKU = product.ProductSKU,
                 Name = product.Name,
-                CityProd = product.CityProd,
-                DateProd = product.DateProd,
                 PriceProd = product.PriceProd,
-                IsCertificatePresent = product.IsCertificatePresent,
-                ExemplarsDTO = product.Exemplars.Select(exemplar => new ProductExemplarDTO
-                {
-                    Code = exemplar.Code,
-                    ProductSKU = exemplar.ProductSKU,
-                    RowNumber = exemplar.RowNumber,
-                    CellNumber = exemplar.CellNumber
-                }).ToList()
+                NumbOfCopies = product.Exemplars.Count(),
             }) ;
             return productsDTO;
         }
